923d5d824202b1aae4d4000b3965e11cdd2065c0
aragakerubo <aragakerubo@gmail.com>
2023-04-27 07:18:31 -0700

    double fibonacci(int n)

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+double fibonacci(int n);
+
+/**
+ * main - Entry point
+ *
+ * Description: Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	double fib, sum = 0;
+
+	for (i = 0; i <= 33; i++)
+	{
+		fib = fibonacci(i);
+		sum = sum + fib;
+	}
+	printf("%.0f\n", sum);
+	return (0);
+}
+
+/**
+ * fibonacci - Computes the n-th Fibonacci number
+ * @n: the integer position
+ *
+ * Description: Computes the n-th Fibonacci number
+ *
+ * Return: Returns the n-th Fibonacci number
+ */
+double fibonacci(int n)
+{
+	if (n <= 1)
+		return (n);
+	return (fibonacci(n - 1) + fibonacci(n - 2));
+}
