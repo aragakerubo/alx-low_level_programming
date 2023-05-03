f0748eec175783cecf2d8e63de8f0105cef080f1
aragakerubo <aragakerubo@gmail.com>
2023-05-03 05:08:41 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,12 +13,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (str[i] != '\0')
 	{
-		if (str[i] == '\0')
-			break;
-		_putchar(*(str + i));
-		i += 2;
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+		i++;
 	}
 	_putchar('\n');
 }
