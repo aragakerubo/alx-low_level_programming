cb41f9e61fb16fe289c07e6b01f4104e448c731b
aragakerubo <aragakerubo@gmail.com>
2023-05-04 12:26:21 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Description: compares two strings
+ *
+ * Return: 0 if equal, -15 if s1 < s2, 15 if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (i > j)
+	{
+		k = 15;
+	}
+	else if (i < j)
+	{
+		k = -15;
+	}
+	else
+	{
+		k = 0;
+	}
+	return (k);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -6,4 +6,6 @@
 
 ### 2. strncpy
 
+### 3. strcmp
+
 
