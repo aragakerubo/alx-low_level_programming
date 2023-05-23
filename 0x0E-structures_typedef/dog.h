ab5258dce95bb99e807a7675c033cd7b1b4371a0
aragakerubo <aragakerubo@gmail.com>
2023-05-23 03:27:20 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: a dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
