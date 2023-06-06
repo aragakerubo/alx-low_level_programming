61c7c49e5a3c4f7f08fd9d1fc589dbb008cca398
aragakerubo <aragakerubo@gmail.com>
2023-06-06 06:31:41 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new)
 	{
 		new->n = n;
-		if (i == 0)
+		new->next = NULL;
+
+		if (idx == 0)
 		{
 			new->next = *head;
 			*head = new;
@@ -28,16 +30,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 		{
 			current = *head;
-			while (i < idx && current != NULL)
+			while (i < idx - 1 && current != NULL)
 			{
 				current = current->next;
 				i++;
 			}
-			if (i < idx)
+			if (i < idx - 1)
 			{
-				new->next = current->next;
-				current->next = new;
+				free(new);
+				return (NULL);
 			}
+			new->next = current->next;
+			current->next = new;
 		}
 	}
 	return (new);
