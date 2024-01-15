8a0b35fbaf5326a2e840028f1df3de5bac3a3f16
aragakerubo <aragakerubo@gmail.com>
2024-01-15 15:01:42 -0500

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,12 +8,12 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t num_nodes = 0;
+	size_t num_nodes = 0;
 
-    while (h)
-    {
-        num_nodes++;
-        h = h->next;
-    }
-    return (num_nodes);
+	while (h)
+	{
+		num_nodes++;
+		h = h->next;
+	}
+	return (num_nodes);
 }
