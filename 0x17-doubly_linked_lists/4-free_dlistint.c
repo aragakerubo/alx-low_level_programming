9e4d29d0b4c9764ef0750da7d3d75fd4ed32b306
aragakerubo <aragakerubo@gmail.com>
2024-01-15 16:51:50 -0500

    4. Free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,12 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-        dlistint_t *next_node = NULL;
+	dlistint_t *next_node = NULL;
 
-        while (head)
-        {
-                next_node = head->next;
-                free(head);
-                head = next_node;
-        }
+	while (head)
+	{
+		next_node = head->next;
+		free(head);
+		head = next_node;
+	}
 }
