d43458c329b3e932f21431e0e0b192d6a1fdc05d
aragakerubo <aragakerubo@gmail.com>
2024-02-04 21:09:03 -0500

    6. >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table
+ * @ht: hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
