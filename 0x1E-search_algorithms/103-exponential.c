e5c303c45bdd99e15aca99b6f5c02bba53a123ac
aragakerubo <aragakerubo@gmail.com>
2024-05-09 16:01:56 -0400

    10. Exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -17,12 +17,32 @@ int exponential_search(int *array, size_t size, int value)
 
 	while (bound < size && array[bound] < value)
 	{
-		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		printf("Value checked array[%lu] = [%d]\n",
+		       bound, array[bound]);
 		bound *= 2;
 	}
 
 	left = bound / 2;
 	right = (bound < size) ? bound : size - 1;
 	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
-	return (binary_search(array + left, right - left + 1, value) + left);
+
+	while (left <= right)
+	{
+		size_t mid = (left + right) / 2;
+
+		printf("Searching in array: ");
+		for (size_t i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[right]);
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
 }
