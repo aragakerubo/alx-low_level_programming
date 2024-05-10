6709ae34c8b55a3a74050efdcb4abc0b420a68b7
aragakerubo <aragakerubo@gmail.com>
2024-05-09 23:00:26 -0400

    10. Exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -11,43 +11,31 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t bound, left, right, mid;
+	size_t bound, i;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
-	if (array[0] == value)
-		return (0);
+
+	if (size == 0)
+		return (-1);
 
 	bound = 1;
 	while (bound < size && array[bound] < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n",
-		       bound, array[bound]);
+		       bound,
+		       array[bound]);
 		bound *= 2;
 	}
-
-	left = bound / 2;
-	right = (bound < size) ? bound : size - 1;
 	printf("Value found between indexes [%lu] and [%lu]\n",
-	       left, right);
-
-	while (left <= right)
+	       bound / 2, bound);
+	if (bound >= size)
+		bound = size - 1;
+	for (i = bound / 2; i <= bound; i++)
 	{
-		mid = (left + right) / 2;
-
-		printf("Searching in array: ");
-		for (size_t i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[right]);
-
-		if (array[mid] == value)
-			return (mid);
-
-		if (array[mid] < value)
-			left = mid + 1;
-		else
-			right = mid - 1;
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
-
 	return (-1);
 }
