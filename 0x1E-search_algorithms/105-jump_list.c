9489092a724a73a9b94091578f04f6cce839ed89
aragakerubo <aragakerubo@gmail.com>
2024-05-10 15:31:30 -0400

    11. Advanced binary search

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -11,36 +11,31 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t i, jump;
-	listint_t *prev, *node;
+	size_t step, step_size;
+	listint_t *node, *jump;
 
-	if (list == NULL)
+	if (list == NULL || size == 0)
 		return (NULL);
 
-	jump = sqrt(size);
-	prev = list;
-	node = list;
-	for (i = 0; i < size; i += jump)
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
 	{
-		if (i != 0)
-			prev = prev->next;
-		while (node->index < i)
-			node = node->next;
-		printf("Value checked at index [%lu] = [%d]\n",
-		       node->index, node->n);
-		if (node->n >= value)
-			break;
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
+		{
+			if (jump->index + 1 == size)
+				break;
+		}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
 	}
-	printf("Value found between indexes [%lu] and [%lu]\n",
-	       prev->index, node->index);
-	printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-	while (prev->index <= node->index && prev != NULL)
-	{
-		printf("Value checked at index [%lu] = [%d]\n",
-		       prev->index, prev->n);
-		if (prev->n == value)
-			return (prev);
-		prev = prev->next;
-	}
-	return (NULL);
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+	       node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
 }
